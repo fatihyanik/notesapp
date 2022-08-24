b08fac3e50dd78b4aba97e91e3b6f882a2dd61df
fatihyanik <69148086+fatihyanik@users.noreply.github.com>
2022-08-24 15:25:45 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,8 @@ import Header from './components/Header';
 
 const App = () => {
 	const [notes, setNotes] = useState([]);
-
 	const [searchText, setSearchText] = useState('');
-
 	const [darkMode, setDarkMode] = useState(false);
-
 	useEffect(() => {
 		const savedNotes = JSON.parse(
 			localStorage.getItem('react-notes-app-data')
@@ -20,14 +17,12 @@ const App = () => {
 			setNotes(savedNotes);
 		}
 	}, []);
-
 	useEffect(() => {
 		localStorage.setItem(
 			'react-notes-app-data',
 			JSON.stringify(notes)
 		);
 	}, [notes]);
-
 	const addNote = (text) => {
 		const date = new Date();
 		const newNote = {
@@ -38,12 +33,10 @@ const App = () => {
 		const newNotes = [...notes, newNote];
 		setNotes(newNotes);
 	};
-
 	const deleteNote = (id) => {
 		const newNotes = notes.filter((note) => note.id !== id);
 		setNotes(newNotes);
 	};
-
 	return (
 		<div className={`${darkMode && 'dark-mode'}`}>
 			<div className='container'>
